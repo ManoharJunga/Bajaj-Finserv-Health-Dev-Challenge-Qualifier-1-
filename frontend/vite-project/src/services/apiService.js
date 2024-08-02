122024261da171636e469b95a4beda350c83941b
ManoharJunga <95352064+ManoharJunga@users.noreply.github.com>
2024-08-02 20:27:50 +0530

    commit

--- frontend/vite-project/src/services/apiService.js
+++ frontend/vite-project/src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/bfhl';
+const API_URL = 'https://backend-bajaj-finserv.vercel.app/bfhl';
 
 export const submitData = async (data) => {
     try {
